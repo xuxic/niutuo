dde57248490a08fb09e4be1f63fd144be9bdbe92
xi-cheng.xu <xi-cheng.xu@hp.com>
2015-04-26 18:25:54 +0800

    use gb2312 for output.csv

--- static.py
+++ static.py
@@ -19,12 +19,12 @@ class signup:
     def POST(self):
         i = web.input()
         print i
-        print i.name.encode('utf8')
-        print i.gender.encode('utf8')
-        print i.phone.encode('utf8')
+        #print i.name.encode('utf8')
+        #print i.gender.encode('utf8')
+        #print i.phone.encode('utf8')
         output = open('static/output.csv','a')
         fcntl.flock(output, fcntl.LOCK_EX)
-        output.write(i.name+','+i.gender+','+i.phone+'\n')
+        output.write(i.name.encode('gb2312')+','+i.gender.encode('gb2312')+','+i.phone.encode('gb2312')+'\r\n')
         output.flush()
         fcntl.flock(output, fcntl.LOCK_UN)
         output.close()
